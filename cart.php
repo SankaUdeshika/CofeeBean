5ca7ad832989c820b4d88b6ab5f62837c2d1fe5e
Sanka Udeshika <sankaudeshika123@gmail.com>
2024-01-11 02:41:36 +0530

    Done Read Product and get Total Prosess

--- cart.php
+++ cart.php
@@ -20,12 +20,37 @@ require "connection.php";
 
         <?php
 
-        $product_rs = Database::search("SELECT * FORM `cart` ");
+        $product_rs = Database::search("SELECT * FROM `cart` INNER JOIN `coffee` ON `cart`.`Coffee_CoffeeId` = `coffee`.`CoffeeId` ");
+        $product_num = $product_rs->num_rows;
+
+        $total = 0;
+
+        for ($i = 0; $i < $product_num; $i++) {
+            $product_data = $product_rs->fetch_assoc();
+            $total = $total + $product_data["price"] * $product_data["qty"];
+        ?>
+            <div>
+                <img src="<?php echo ($product_data["CoffeImagePath"]) ?>" style="width: 100px; height: auto;" alt="">
+                <span><?php echo ($product_data["CoffeeName"]) ?></span>
+                <span> X <?php echo ($product_data["price"]) ?></span>
+                <input type="number" id="qty" value="<?php echo ($product_data["qty"]) ?>">
+                <button>Update quantity</button>
+                <button onclick="addcart(<?php echo ($product_data['Coffee_CoffeeId']) ?>)">Delete from cart</button>
+                <hr>
+            </div>
+        <?php
+        }
+
 
         ?>
 
     </div>
-    <div style="width: 50%;"></div>
+    <div style="width: 20%;">
+        <hr>
+        <h1>Total = <?php echo ($total) ?></h1>
+        <hr>
+        <button>Buy Now</button>
+    </div>
 
 
     <script src="script.js"></script>
